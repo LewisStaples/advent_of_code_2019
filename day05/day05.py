3caa8c91fc3b9486a14a683d283e3ac21885ba4b
Lewis Staples <lewis.staples@gmail.com>
2022-07-13 16:41:32 -0400

    Day 05 Part A working

--- day05/day05.py
+++ day05/day05.py
@@ -10,11 +10,8 @@ class Intcode_Program:
         self.int_code_program = list(map(lambda ch:int(ch), int_code_program_ch))
         self.current_index = 0
 
-        if input_filename == 'input.txt':
-            self.int_code_program[1] = 12
-            self.int_code_program[2] = 2
-
     # For debugging and/or testing
+    # The value in parentheses is what is at the current index
     def return_display(self):
         ret_val = '['
         for i,value in enumerate(self.int_code_program):
@@ -29,37 +26,37 @@ class Intcode_Program:
         return ret_val
 
     # rel_position is how far the index is from self.current_index
-    # levels_indirection is 0 if get the value from the list directly
-    # (and 1 if you get the value from the list, and then go to that index to find that value)
-    def get_value(self, rel_position, levels_indirection):
+    # parameter_mode is 1 if get the value from the list directly
+    # (and 0 if you get the value from the list, and then go to that index to find that value)
+    def get_value(self, rel_position, parameter_mode):
         index = self.current_index + rel_position
-        for i in range(levels_indirection):
+        for i in range((int(parameter_mode)+1)%2):
             index = self.int_code_program[index]
         return self.int_code_program[index]
     
-    # rel_position and levels_indirection are the same as get_value
-    # def set_value(self, rel_position, new_value, levels_indirection):
-    def set_value(self, rel_position, levels_indirection, new_value):
+    # rel_position and parameter_mode are the same as get_value
+    def set_value(self, rel_position, parameter_mode, new_value):
         index = self.current_index + rel_position
-        for i in range(levels_indirection):
+        for i in range((int(parameter_mode)+1)%2):
             index = self.int_code_program[index]
         self.int_code_program[index] = new_value
 
-    def add(self):
-        op1 = self.get_value(1,1)
-        op2 = self.get_value(2,1)
-        # self.set_value(3, op1 + op2, 1)
-        self.set_value(3, 1, op1 + op2)
+    # opcode 1
+    def add(self, parameter_modes):
+        op1 = self.get_value(1,parameter_modes[2])
+        op2 = self.get_value(2,parameter_modes[1])
+        self.set_value(3, parameter_modes[0], op1 + op2)
         self.current_index += 4
 
-    def mult(self):
-        op1 = self.get_value(1,1)
-        op2 = self.get_value(2,1)
-        # self.set_value(3, op1 * op2, 1)
-        self.set_value(3, 1, op1 * op2)
+    # opcode 2
+    def mult(self, parameter_modes):
+        op1 = self.get_value(1,parameter_modes[2])
+        op2 = self.get_value(2,parameter_modes[1])
+        self.set_value(3, parameter_modes[0], op1 * op2)
         self.current_index += 4
 
-    def input(self):
+    # opcode 3
+    def input(self, parameter_modes):
         input_value = None
         try:
             input_value = int(input('Please enter the input value: '))
@@ -67,34 +64,39 @@ class Intcode_Program:
             print('Your input is not a integer.  The program will now end ... please try again!')
             sys.exit(-1)
 
-        self.set_value(1, 1, input_value)
+        # Using 0 as parameter mode, because of below quote from the specification
+        # "Parameters that an instruction writes to will never be in immediate mode."
+        self.set_value(1, 0, input_value)
+        if parameter_modes != "000":
+            print(f'WARNING! parameter mode for input is {parameter_modes} !!!')
 
         self.current_index += 2
 
-    def output(self):
-        output_value = self.get_value(1,1)
-        print(f'The ouput value is: {output_value}')
+    # opcode 4
+    def output(self, parameter_modes):
+        output_value = self.get_value(1,parameter_modes[2])
+        print(f'The output value is: {output_value}')
         self.current_index += 2
 
-    def exit(self):
+    # parameter_modes_dummy is ignored when exiting the program
+    def exit(self, parameter_modes_dummy):
         self.current_index = len(self.int_code_program)
 
     def parse(self):
-        print(f'Initial: {self.return_display()}')
+        # print(f'Initial: {self.return_display()}\n')
         while self.current_index < len(self.int_code_program) - 1:
             opcode_function = {1: 'add', 2:'mult', 3:'input', 4:'output', 99:'exit'}
-            getattr(self, opcode_function[self.get_value(0,0)])()
-            print(f'Values  : {self.return_display()}')
+            op_instruction_str = str(self.int_code_program[self.current_index])
+            opcode = int(op_instruction_str[-2:])
+            parameter_modes = op_instruction_str[:-2].zfill(3)
+            getattr(self, opcode_function[opcode])(parameter_modes)
+            # print(f'Values  : {self.return_display()}\n')
 
 # Reading input from the input file
-input_filename='input_sample0.txt'
+input_filename='input.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     in_string = f.readline().rstrip()
 intcode_program = Intcode_Program(in_string)
 
 intcode_program.parse()
-
-
-
-
