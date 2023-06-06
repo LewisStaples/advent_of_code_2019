c8dc84b33c89c5c8aab0d69cb4180e86e1c75a54
Lewis Staples <lewis.staples@gmail.com>
2023-06-05 23:42:37 -0400

    Day 08 Part B: Getting desired result from sample data, but failing on graded data

--- day08/day08.py
+++ day08/day08.py
@@ -19,6 +19,7 @@ def get_pixel_dimensions(input_filename):
         return (3,2)
     elif input_filename == 'input_sample1.txt':
         return (2,2)
+        # return (3,2) # JUNK, for TESTING only
     else:
         return (25,6)
 
@@ -39,18 +40,68 @@ def get_part_a(pixel_dimensions, input_digits):
     return layer_smallest_zero.count(1) * layer_smallest_zero.count(2)
 
 
-def print_part_b(pixel_dimensions, input_digits):
-    dummy = 123
+def print_image(image):
+    for y in range(len(image)):
+        for x in range(len(image[0])):
+            print (image[x][y], end = '  ')
+        print()
+    print()
 
+def print_part_b(pixel_dimensions, input_digits):
+    LAYER_SIZE = pixel_dimensions[0] * pixel_dimensions[1]
+    image = [list()]
+    # this_row = list()
     input_digits_copy = copy.deepcopy(input_digits)
     while len(input_digits_copy) > 0:
-        print(input_digits_copy.pop(0))
-    # Handle top layer
-    for j in range(pixel_dimensions[1]):
-        # Start another row of pixels:
-        for i in range(pixel_dimensions[0]):
-            # Handle a pixel within that row
+        # print(input_digits_copy.pop(0))
+        this_digit = input_digits_copy.pop(0)
+        # this_row.append(input_digits_copy.pop(0))
+        this_digit_number = len(input_digits) - len(input_digits_copy) - 1
+
+        # If this is the first layer
+        if this_digit_number < LAYER_SIZE:
+            image[-1].append(this_digit)
+            # If this row is full
+            if len(image[-1]) == pixel_dimensions[0]:
+                if this_digit_number < LAYER_SIZE - 1:
+                    image.append(list())
+
+        # For subsequent layers
+        else:
+            # image[-1].append(this_digit)
+            this_col_number = this_digit_number % pixel_dimensions[0]
+            this_row_number = ( this_digit_number // pixel_dimensions[0] ) % pixel_dimensions[1]
+
+            # If the image pixel is transparent
+            if image[this_col_number][this_row_number] == 2:
+                # Put in the new "color" (white, black, or transparent)
+                image[this_col_number][this_row_number] = this_digit
+            dummy = 123
+
             pass
+
+            # # If this row is full
+            # if len(image[-1]) == pixel_dimensions[0]:
+            #     if this_digit_number < LAYER_SIZE - 1:
+            #         image.append(list())
+
+    print_image(image)
+
+    dummy = 123
+
+        # # If it's a new layer
+        # if this_digit_number % LAYER_SIZE == 0:
+        #     pass
+        # If it's a new row
+        # if this_digit_number % pixel_dimensions[0] == 0:
+        #     pass
+
+    # # Handle top layer
+    # for j in range(pixel_dimensions[1]):
+    #     # Start another row of pixels:
+    #     for i in range(pixel_dimensions[0]):
+    #         # Handle a pixel within that row
+    #         pass
 
     # For subsequent layers, only update if the pixel starts off transparent
 
