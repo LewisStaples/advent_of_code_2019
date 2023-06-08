18916f599ed5e992e5e8b22d8d847129c7e4c174
Lewis Staples <lewis.staples@gmail.com>
2023-06-07 22:21:12 -0400

    Day 08 Part B: Fixed error ... giving correct results now.

--- day08/day08.py
+++ day08/day08.py
@@ -19,7 +19,6 @@ def get_pixel_dimensions(input_filename):
         return (3,2)
     elif input_filename == 'input_sample1.txt':
         return (2,2)
-        # return (3,2) # JUNK, for TESTING only
     else:
         return (25,6)
 
@@ -43,19 +42,20 @@ def get_part_a(pixel_dimensions, input_digits):
 def print_image(image):
     for y in range(len(image)):
         for x in range(len(image[0])):
-            print (image[x][y], end = '  ')
+            if image[y][x] == 1:
+                print('X', end = '')
+            else:
+                print(' ', end = '')
         print()
     print()
 
 def print_part_b(pixel_dimensions, input_digits):
+    print('Printing part B message (1 is displayed as "X," all others are not displayed):\n')
     LAYER_SIZE = pixel_dimensions[0] * pixel_dimensions[1]
     image = [list()]
-    # this_row = list()
     input_digits_copy = copy.deepcopy(input_digits)
     while len(input_digits_copy) > 0:
-        # print(input_digits_copy.pop(0))
         this_digit = input_digits_copy.pop(0)
-        # this_row.append(input_digits_copy.pop(0))
         this_digit_number = len(input_digits) - len(input_digits_copy) - 1
 
         # If this is the first layer
@@ -68,44 +68,13 @@ def print_part_b(pixel_dimensions, input_digits):
 
         # For subsequent layers
         else:
-            # image[-1].append(this_digit)
-            this_col_number = this_digit_number % pixel_dimensions[0]
-            this_row_number = ( this_digit_number // pixel_dimensions[0] ) % pixel_dimensions[1]
+            this_row_number = this_digit_number % pixel_dimensions[0]
+            this_col_number = ( this_digit_number // pixel_dimensions[0] ) % pixel_dimensions[1]
 
-            try:
-                # If the image pixel is transparent
-                if image[this_col_number][this_row_number] == 2:
-                    # Put in the new "color" (white, black, or transparent)
-                    image[this_col_number][this_row_number] = this_digit
-                dummy = 123
-            except IndexError:
-                dummy = 123
-
-            # # If this row is full
-            # if len(image[-1]) == pixel_dimensions[0]:
-            #     if this_digit_number < LAYER_SIZE - 1:
-            #         image.append(list())
+            if image[this_col_number][this_row_number] == 2:
+                image[this_col_number][this_row_number] = this_digit
 
     print_image(image)
-
-    dummy = 123
-
-        # # If it's a new layer
-        # if this_digit_number % LAYER_SIZE == 0:
-        #     pass
-        # If it's a new row
-        # if this_digit_number % pixel_dimensions[0] == 0:
-        #     pass
-
-    # # Handle top layer
-    # for j in range(pixel_dimensions[1]):
-    #     # Start another row of pixels:
-    #     for i in range(pixel_dimensions[0]):
-    #         # Handle a pixel within that row
-    #         pass
-
-    # For subsequent layers, only update if the pixel starts off transparent
-
 
 
 def solve_problem(input_filename):
@@ -116,6 +85,3 @@ def solve_problem(input_filename):
     print_part_b(pixel_dimensions, input_digits)
 
 solve_problem('input.txt')
-
-
-
