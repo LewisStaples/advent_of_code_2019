2e701c8ad3fc2c7c9b5dc1de9ebe276f6f09fd5a
Lewis Staples <lewis.staples@gmail.com>
2023-06-11 23:48:51 -0400

    Day 10  Part 2: Changing output of Part 1 to retain coordinates of the chosen new location for the monitoring station.  Set up skeleton for part 2 logic.

--- day10/day10.py
+++ day10/day10.py
@@ -6,7 +6,7 @@
 
 import math
 
-
+# This function is for part 1
 def get_asteroid_points(input_filename):
     asteroid_points = set()
     # Reading input from the input file
@@ -15,7 +15,7 @@ def get_asteroid_points(input_filename):
         # Pull in each line from the input file
         for row_number, in_string in enumerate(f):
             in_string = in_string.rstrip()
-            print(f'{row_number}: {in_string}')
+            print(f'{row_number}: {in_string}', end = '')
             for col_number, in_char in enumerate(in_string):
                 if in_char == '#':
                     asteroid_points.add((col_number, row_number))
@@ -24,7 +24,7 @@ def get_asteroid_points(input_filename):
 
     return asteroid_points
 
-
+# This function is for part 1
 def visible(asteroid, monitoring_station_point, asteroid_points):
     # Find greatest common factor
     dx = - asteroid[0] + monitoring_station_point[0]
@@ -42,7 +42,7 @@ def visible(asteroid, monitoring_station_point, asteroid_points):
             return False
     return True
 
-
+# This function is for part 1
 def get_count_observed_asteroids(asteroid_points, monitoring_station_point):
     the_count = 0
     for asteroid in asteroid_points:
@@ -52,14 +52,33 @@ def get_count_observed_asteroids(asteroid_points, monitoring_station_point):
             the_count += 1
     return the_count
 
-def solve_problem(input_filename):
-    asteroid_points = get_asteroid_points(input_filename)
-    max_count_observable_asteroids = 0
-    for monitoring_station_point in asteroid_points:
-        max_count_observable_asteroids = max(max_count_observable_asteroids, get_count_observed_asteroids(asteroid_points, monitoring_station_point))
-    print(f'The max. asteroids visible is {max_count_observable_asteroids}\n')
 
-solve_problem('input.txt')
+def vaporize_next_asteroid(asteroid_points, laser_angle):
+    # Need to destroy asteroid at laser_angle, if any
+    pass
+
+
+def solve_problem(input_filename):
+    # Code to solve part 1
+    asteroid_points = get_asteroid_points(input_filename)
+    max_count_observable_asteroids = -1
+    for monitoring_station_point in asteroid_points:
+        # max_count_observable_asteroids = max(max_count_observable_asteroids, get_count_observed_asteroids(asteroid_points, monitoring_station_point))
+        new_asteroid_count = get_count_observed_asteroids(asteroid_points, monitoring_station_point)
+        if new_asteroid_count > max_count_observable_asteroids:
+            max_count_observable_asteroids = new_asteroid_count
+
+    print(f'The max. asteroids visible (part 1) is {max_count_observable_asteroids}\n')
+
+    # Code to solve part 2
+    # Initialize angle to point upward
+    laser_angle = 0
+    # for i in range(200):
+    for i in range(5): # For testing only
+        vaporize_next_asteroid(asteroid_points, laser_angle)
+
+
+solve_problem('input_sample0.txt')
 
 # def test_sample_0():
 #     solve_problem('input_sample0.txt')
