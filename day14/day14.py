794ca9d17d65262f48db55c36a62fe0d9aee1493
Lewis Staples <lewis.staples@gmail.com>
2023-06-28 23:25:48 -0400

    Day 14:  Implemented some recursion logic, which stores quantities as ratios of whole numbers (thereby avoiding floating point errors).  However, I will try a different path tomorrow, but I wanted a record of what I did.

--- day14/day14.py
+++ day14/day14.py
@@ -5,6 +5,8 @@
 
 
 from dataclasses import dataclass
+import math
+
 
 @dataclass
 class ChemicalAndQuantity:
@@ -42,20 +44,39 @@ def get_input(input_filename):
     return reactions
 
 # recursive function
-# def take_next_recursive_step(quantity, chemical):
-    # if chemical == 'ORE'
+def take_next_recursive_step(quantity, chemical, the_reactions):
+    if chemical == 'ORE':
     #   return quantity of 'ORE'
-    # else:
-    #   return take_next_recursive_step(quantity, chemical)
+      return quantity
+    else:
+        dummy = 123
+        ret_val_quantity = [0,1]
+        for reactant in the_reactions[chemical].reactants:
+            quantity_added = take_next_recursive_step(
+                [reactant.quantity,1], 
+                reactant.chemical, 
+                the_reactions
+            )
+            rvq_denominator = math.lcm(ret_val_quantity[1], quantity_added[1])
+            rvq_numerator = rvq_denominator * ret_val_quantity[0] / ret_val_quantity[1] + rvq_denominator * quantity_added[0] / quantity_added[1]
+            ret_val_quantity = [rvq_numerator, rvq_denominator]
 
+    #   return take_next_recursive_step(quantity, chemical, the_reactions)
+    return ret_val_quantity
 def get_stepcount(the_reactions):
     # Start a recursive march through all chemical reaction steps
-    # Start with 1 'FUEL'
-    return 42
+    the_numerator, the_denominator = take_next_recursive_step( 
+        [1,1], 'FUEL', # Start with 1 'FUEL':  the quantity is 1,1 to reflect fraction 1/1
+        the_reactions
+    )
+    assert the_numerator % the_denominator == 0
+    return int(the_numerator / the_denominator)
 
 def solve_problem(input_filename):
+    # Part one logic
     the_reactions = get_input(input_filename)
     stepcount = get_stepcount(the_reactions)
+    print(f'Part 1 answer: {stepcount}')
 
 
-solve_problem('input.txt')
+solve_problem('input_sample0.txt')
