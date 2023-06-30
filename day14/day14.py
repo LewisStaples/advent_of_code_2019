045b78e5cd78a7f2c1f8078fa918768a7f52865d
Lewis Staples <lewis.staples@gmail.com>
2023-06-29 23:55:13 -0400

    Day 14:  Implementation of revised logic is in progress. Tallying up amounts needed for a given line, with those from other lines .... Later on, I intend to handle combining these needed amounts to reduce consuming more than what is needed.

--- day14/day14.py
+++ day14/day14.py
@@ -5,21 +5,19 @@
 
 
 from dataclasses import dataclass
-import math
 
-
-@dataclass
+# @dataclass
 class ChemicalAndQuantity:
-    chemical: str
-    quantity: int
+    # chemical: str
+    # quantity: int
     def __init__(self, caq_str):
         quantity_str, self.chemical = caq_str.split(' ')
         self.quantity = int(quantity_str)
 
-@dataclass
+# @dataclass
 class ReactionData:
-    product: ChemicalAndQuantity
-    reactants: list
+    # product: ChemicalAndQuantity
+    # reactants: list
     def __init__(self, reaction_str):
         self.reactants = list()
         reactant_str, product_str = reaction_str.split(' => ')
@@ -27,8 +25,18 @@ class ReactionData:
         for reactant_substr in reactant_str.split(', '):
             self.reactants.append(ChemicalAndQuantity(reactant_substr))
 
+def do_reactant_counting(count_reactions_used_in, the_reaction_data):
+    for the_reaction in the_reaction_data.reactants:
+        dummy = 123
+        if the_reaction.chemical in count_reactions_used_in:
+            count_reactions_used_in[the_reaction.chemical] += 1
+        else:
+            count_reactions_used_in[the_reaction.chemical] = 1
+        dummy = 123
+
 def get_input(input_filename):
     reactions = dict()
+    count_reactions_used_in = {'FUEL': 0} # dict()
     print(f'\nUsing input file: {input_filename}\n')
     with open(input_filename) as f:
         # Pull in each line from the input file
@@ -38,45 +46,38 @@ def get_input(input_filename):
 
             the_reaction_data = ReactionData(in_string)
             reactions[the_reaction_data.product.chemical] = the_reaction_data
+
+            do_reactant_counting(count_reactions_used_in, the_reaction_data)
             dummy = 123
 
     print()
-    return reactions
+    return reactions, count_reactions_used_in
 
 # recursive function
-def take_next_recursive_step(quantity, chemical, the_reactions):
-    if chemical == 'ORE':
+# def take_next_recursive_step(quantity, chemical):
+    # if chemical == 'ORE'
     #   return quantity of 'ORE'
-      return quantity
-    else:
-        dummy = 123
-        ret_val_quantity = [0,1]
-        for reactant in the_reactions[chemical].reactants:
-            quantity_added = take_next_recursive_step(
-                [reactant.quantity,1], 
-                reactant.chemical, 
-                the_reactions
-            )
-            rvq_denominator = math.lcm(ret_val_quantity[1], quantity_added[1])
-            rvq_numerator = rvq_denominator * ret_val_quantity[0] / ret_val_quantity[1] + rvq_denominator * quantity_added[0] / quantity_added[1]
-            ret_val_quantity = [rvq_numerator, rvq_denominator]
+    # else:
+    #   return take_next_recursive_step(quantity, chemical)
 
-    #   return take_next_recursive_step(quantity, chemical, the_reactions)
-    return ret_val_quantity
-def get_stepcount(the_reactions):
-    # Start a recursive march through all chemical reaction steps
-    the_numerator, the_denominator = take_next_recursive_step( 
-        [1,1], 'FUEL', # Start with 1 'FUEL':  the quantity is 1,1 to reflect fraction 1/1
-        the_reactions
-    )
-    assert the_numerator % the_denominator == 0
-    return int(the_numerator / the_denominator)
+def get_stepcount(the_reactions, count_reactions_used_in):
+    # Start with 1 'FUEL'
+    quantity_needed = {'FUEL':1}
+    while len(count_reactions_used_in) > 0:
+        chemical_list = list(count_reactions_used_in.keys())
+        count_list = list(count_reactions_used_in.values())
+        the_index = count_list.index(0)
+        the_chemical = chemical_list[the_index]
+        del count_reactions_used_in[the_chemical]
+
+        # ADD TO quantity_needed
+        # REDUCE count_reactions_used_in
+
+        return 42 # Dummy command to prevent infinite loop
 
 def solve_problem(input_filename):
-    # Part one logic
-    the_reactions = get_input(input_filename)
-    stepcount = get_stepcount(the_reactions)
-    print(f'Part 1 answer: {stepcount}')
+    the_reactions, count_reactions_used_in = get_input(input_filename)
+    stepcount = get_stepcount(the_reactions, count_reactions_used_in)
 
 
 solve_problem('input_sample0.txt')
