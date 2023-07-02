6f153504423cb700465be057a77c7c4839ec4efd
Lewis Staples <lewis.staples@gmail.com>
2023-07-01 23:24:29 -0400

    Day 14:  Getting correct answer for Part 1

--- day14/day14.py
+++ day14/day14.py
@@ -5,6 +5,7 @@
 
 
 from dataclasses import dataclass
+import math
 
 # @dataclass
 class ChemicalAndQuantity:
@@ -63,30 +64,43 @@ def get_input(input_filename):
 def get_stepcount(the_reactions, count_reactions_used_in):
     # Start with 1 'FUEL'
     quantity_needed = {'FUEL':1}
-    while len(count_reactions_used_in) > 0:
+    while len(count_reactions_used_in) > 1: # 0:
+    # while True:
         chemical_list = list(count_reactions_used_in.keys())
         count_list = list(count_reactions_used_in.values())
         the_index = count_list.index(0)
         the_chemical = chemical_list[the_index]
         del count_reactions_used_in[the_chemical]
 
+        # if len(count_reactions_used_in) == 0:
+            # break
+        # if the_chemical == 'ORE':
+            # dummy = 123
+
         # ADD TO quantity_needed
         for the_reactant in the_reactions[the_chemical].reactants:
-            the_reactant_quantity = the_reactant.quantity  # NEEDED ... multiple needed quantity_needed[the_chemical] , quantity in the_reactions[the_chemical]
+            the_reactant_quantity_needed = the_reactant.quantity * math.ceil(quantity_needed[the_chemical] / the_reactions[the_chemical].product.quantity)
+                # the_reactions[the_chemical].product.quantity == 10
+                # 
             if the_reactant.chemical not in quantity_needed:
-                quantity_needed[the_reactant.chemical] = the_reactant_quantity
+                quantity_needed[the_reactant.chemical] = the_reactant_quantity_needed
             else:
-                quantity_needed[the_reactant.chemical] += the_reactant_quantity
+                quantity_needed[the_reactant.chemical] += the_reactant_quantity_needed
 
             dummy = 123
+            # REDUCE count_reactions_used_in
+            count_reactions_used_in[the_reactant.chemical] -= 1
+            dummy = 123
 
-        # REDUCE count_reactions_used_in
+        dummy = 123
+        # return 42 # Dummy command to prevent infinite loop
+    dummy = 123
+    return quantity_needed['ORE']
 
-        return 42 # Dummy command to prevent infinite loop
 
 def solve_problem(input_filename):
     the_reactions, count_reactions_used_in = get_input(input_filename)
     stepcount = get_stepcount(the_reactions, count_reactions_used_in)
-
+    print(f'The answer to Part One is: {stepcount}\n')
 
 solve_problem('input_sample0.txt')
