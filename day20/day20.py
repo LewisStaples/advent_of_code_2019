7d146bb5b5c507c5b1f7a9511dc96c6c717cf3e2
Lewis Staples <lewis.staples@gmail.com>
2023-07-14 17:49:04 -0400

    Day 20:  Part 1:  converted datatype of portal names from indices in a dict to a set

--- day20/day20.py
+++ day20/day20.py
@@ -13,7 +13,8 @@ def get_input(input_filename):
         # This dict will have key column_number, and value letter seen (first character in a portal)
         # It is assumed that portal names are always two letters (sometimes horizontal, and sometimes they are vertical)
         cols_with_letters_to_finish = dict()
-        portals = dict()
+        # portals = dict()
+        portals = set()
         # Pull in each line from the input file
         for row_number, in_string in enumerate(f):
             in_string = in_string.rstrip()
@@ -25,15 +26,17 @@ def get_input(input_filename):
                     if col_number > 0:
                         # try:
                         if in_string[col_number - 1].isalpha():
-                            portals[in_string[col_number-1:col_number + 1]] = None
+                            # portals[in_string[col_number-1:col_number + 1]] = None
+                            portals.add(in_string[col_number-1:col_number + 1])
                             del cols_with_letters_to_finish[col_number - 1]
                             continue
                             dummy = 123
 
                     if col_number in cols_with_letters_to_finish:
-                        portals[
-                            cols_with_letters_to_finish[col_number] + in_string[col_number]
-                        ] = None
+                        # portals[
+                        #     cols_with_letters_to_finish[col_number] + in_string[col_number]
+                        # ] = None
+                        portals.add(cols_with_letters_to_finish[col_number] + in_string[col_number])
                         del cols_with_letters_to_finish[col_number]
                     else:
                         cols_with_letters_to_finish[col_number] = in_string[col_number]
