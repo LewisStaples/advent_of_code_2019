5acab86853d891828e39634040fc8f7c786fadd3
Lewis Staples <lewis.staples@gmail.com>
2023-08-04 23:42:38 -0400

    Day 22: Defined data structure for deck.  First of three commands implemented by reversing the list of numbers

--- day22/day22.py
+++ day22/day22.py
@@ -5,14 +5,24 @@
 
 class Command:
     def __init__(self, in_string):
-        number_parameter = None
+        self.number_parameter = None
         if in_string[-1].isdigit():
             self.in_string, number_parameter_str = in_string.rsplit(' ', 1)
             self.number_parameter = int(number_parameter_str)
-            dummy = 123
+        else:
+            self.in_string = in_string
+
+
+class Deck:
+    def __init__(self, num_cards):
+        self.cards = [card_val for card_val in range(num_cards)]
+
+    def display(self):
+        return ' '.join([str(card) for card in self.cards])
+        # return text(self.cards)
 
 def get_input(input_filename):
-    the_input = list()
+    the_commands = list()
     # Reading input from the input file
     print(f'\nUsing input file: {input_filename}\n')
     with open(input_filename) as f:
@@ -20,11 +30,32 @@ def get_input(input_filename):
         for in_string in f:
             in_string = in_string.rstrip()
             print(in_string)
-            the_input.append(Command(in_string))
+            the_commands.append(Command(in_string))
+    print()
+    return the_commands
 
-    
+
+def run(command, deck):
+    dummy = 123
+    if command.in_string == 'deal into new stack':
+        deck.cards.reverse()
+    dummy = 123
+
+
 def solve_problem(input_filename):
-    the_input = get_input(input_filename)
+    the_commands = get_input(input_filename)
+    deck = None
+    if len(the_commands) < 75:
+        # Sample data
+        deck = Deck(10)
+    else:
+        # The real judged data
+        deck = Deck(10007)
 
-solve_problem('input.txt')
+    print(f'Initial deck:  {deck.display()}')
+    for command in the_commands:
+        run(command, deck)
+    print(f'Final deck:    {deck.display()}')
+
+solve_problem('input_sample0.txt')
 
